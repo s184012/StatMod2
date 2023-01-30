001e18d66f1863503855871310c33b8b00563708
Simon Majgaard <simon.majgaard@gmail.com>
2023-01-30 16:02:43 +0100

    update explore and make init

--- Assignment1/explore.r
+++ Assignment1/explore.r
@@ -1,3 +1,3 @@
-library()
-setwd(strcat(getwd(), "/Assignment"))
+library('tidyverse')
 dat <- read.table("dioxin.csv", sep=',', head=TRUE)
+str(dat)
---
+++ init.r
@@ -0,0 +1,2 @@
+working_directory <- '/home/simon/Code/StatMod2/Assignment1'
+setwd(working_directory)
