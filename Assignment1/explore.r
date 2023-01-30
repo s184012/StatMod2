c39a65929d89608e1719a2a23d09b4e61d8d352a
Mona Lisa <S204243@student.dtu.dk>
2023-01-30 16:08:14 +0100

    Trying things out

--- Assignment1/explore.r
+++ Assignment1/explore.r
@@ -1,3 +1,16 @@
 library()
-setwd(strcat(getwd(), "/Assignment"))
+#setwd('/Users/OscarBP/Documents/5. DTU noter/Semester 6/02424/Assignments/StatMod2/Assignment1')
+
 dat <- read.table("dioxin.csv", sep=',', head=TRUE)
+
+alpha <- 0.05
+
+
+#DIOX
+
+#BLOCK EFFECTS
+    #PLANT
+    #TIME
+    #LAB
+
+#Active variables
