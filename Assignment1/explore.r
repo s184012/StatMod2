a400ef6770d2bbe83b63abe8378820962d5ec957
Simon Majgaard <simon.majgaard@gmail.com>
2023-02-27 09:29:30 +0100

    Update exploration

--- Assignment1/explore.r
+++ Assignment1/explore.r
@@ -1,5 +1,14 @@
 library('tidyverse')
-dat <- read.table("dioxin.csv", sep=',', head=TRUE)
+library('MASS')
+dat <- read.table("dioxin.csv", sep=',', head=TRUE, stringsAsFactors = TRUE)
+dat$TIME <- as.factor(dat$TIME)
+
+
+#dat <- dat %>%
+ # group_by(PLANT) #%>%
+#  mutate(DIOX=(DIOX - mean(DIOX, na.rm = T)) / sd(DIOX, na.rm=T))
+
+
 
 alpha <- 0.05
 
@@ -12,13 +21,34 @@ alpha <- 0.05
     #LAB
 
 #Active variables
+scaleFUN <- function(x) sprintf("%.2f", x)
+
+
+
+
+factor(x=c("KARA", "RENO_N", "RENO_S"), levels = levels(dat$PLANT))
+
 
 
 dat %>%
     filter(
-        TIME == 1
+        #TIME =
     ) %>%
-    ggplot(aes(x=DIOX)) +
-    geom_density() +
-    facet_grid(cols = vars(PLANT), rows = vars(LAB))
+    ggplot(aes(x=DIOX, fill=TIME)) +
+    geom_density(position = 'stack') +
+    facet_grid(rows = vars(LAB), cols = vars(PLANT))
+    #scale_x_continuous(trans="log", labels = scaleFUN)
+
+dat %>% 
+  ggplot(aes(x=DIOX)) +
+  geom_density(alpha=0.4) 
+
+dat %>%
+  filter(
+    LAB=="KK",
+  ) %>%
+  ggplot(aes(x=DIOX, fill=PRSEK)) +
+  geom_histogram(alpha=.4) 
+                     
+boxcox(lm(data = dat, DIOX ~1))
 
--- w1e2.R
+++
@@ -1,6 +0,0 @@
-df <- read.table("w1e2_Data.txt", header = T)
-df
-
-mdl <- lm(y ~x, df)
-mdl
-
