5d3a85c726d30b473ad78887ae6a6c94330d0f51
Lolz14 <lorenzo.lefosse@gmail.com>
2023-03-09 19:56:12 +0100

    jo

--- proj.r
+++ proj.r
@@ -237,7 +237,7 @@ X = model.matrix( mod15)
 lev = hat(X)
 p = mod15$rank;
 n = length(DIOX);
-plot( mod13$fitted.values, lev, ylab = "Leverages", main = "Plot of Leverages",
+plot( mod15$fitted.values, lev, ylab = "Leverages", main = "Plot of Leverages",
       pch = 16, col = 'black' )
 abline( h = 2 * p/n, lty = 2, col = 'red' )
 watchout_points_lev = lev[ which( lev > 2 * p/n ) ]
@@ -245,48 +245,48 @@ watchout_ids_lev = seq_along( lev )[ which( lev > 2 * p/n ) ]
 points( mod$fitted.values[ watchout_ids_lev ], watchout_points_lev, col = 'red', pch = 16 )
 
 # RESIDUAL ANALYSIS
-gs = summary(mod13)
-res_std = mod13$residuals/sd(DIOX)
+gs = summary(mod15)
+res_std = mod15$residuals/sd(DIOX)
 watchout_ids_rstd = which( abs( res_std ) > 2 )
 watchout_rstd = res_std[ watchout_ids_rstd ]
 watchout_rstd
-plot( mod13$fitted.values, res_std, ylab = "Standardized Residuals", main = "Standardized Residuals" )
+plot( mod15$fitted.values, res_std, ylab = "Standardized Residuals", main = "Standardized Residuals" )
 abline( h = c(-2,2), lty = 2, col = 'orange' )
-points( mod13$fitted.values[watchout_ids_rstd],
+points( mod15$fitted.values[watchout_ids_rstd],
         res_std[watchout_ids_rstd], col = 'red', pch = 16 )
-points( mod13$fitted.values[watchout_ids_lev],
+points( mod15$fitted.values[watchout_ids_lev],
         res_std[watchout_ids_lev], col = 'orange', pch = 16 )
 legend('topright', col = c('red','orange'),
        c('Standardized Residuals', 'Leverages'), pch = rep( 16, 2 ), bty = 'n' )
 # Studentized residuals
-stud = rstudent( mod13 )
+stud = rstudent( mod15 )
 watchout_ids_stud = which( abs( stud ) > 2 )
 watchout_stud = stud[ watchout_ids_stud ]
-plot( mod13$fitted.values, stud, ylab = "Studentized Residuals", main = "Studentized Residuals", pch = 16 )
-points( mod13$fitted.values[watchout_ids_stud],
+plot( mod15$fitted.values, stud, ylab = "Studentized Residuals", main = "Studentized Residuals", pch = 16 )
+points( mod15$fitted.values[watchout_ids_stud],
         stud[watchout_ids_stud], col = 'pink', pch = 16 )
-points( mod13$fitted.values[watchout_ids_lev],
+points( mod15$fitted.values[watchout_ids_lev],
         stud[watchout_ids_lev], col = 'orange', pch = 16 )
 abline( h = c(-2,2), lty = 2, col = 'orange' )
 legend('topright', col = c('pink','orange'),
        c('Studentized Residual', 'Leverages'), pch = rep( 16, 3 ), bty = 'n' )
 
 # Cook Distance
-Cdist = cooks.distance( mod13 )
+Cdist = cooks.distance( mod15 )
 watchout_ids_Cdist = which( Cdist > 4/(n-p) )
 watchout_Cdist = Cdist[ watchout_ids_Cdist ]
 watchout_Cdist
-if(saveFig == TRUE){pdf("Influ.pdf", width = 10*0.8, height = 10*0.8)}
+if(saveFig == TRUE){pdf("Influe.pdf", width = 10*0.8, height = 10*0.8)}
 par( mfrow = c( 1, 3 ) )
-plot( mod13$fitted.values, Cdist, pch = 16, xlab = 'Fitted values',
+plot( mod15$fitted.values, Cdist, pch = 16, xlab = 'Fitted values',
       ylab = 'Cooks Distance', main = 'Cooks Distance' )
-points( mod13$fitted.values[ watchout_ids_Cdist ], Cdist[ watchout_ids_Cdist ],
+points( mod15$fitted.values[ watchout_ids_Cdist ], Cdist[ watchout_ids_Cdist ],
         col = 'green', pch = 16 )
-plot( mod13$fitted.values, stud, pch = 16, xlab = 'Fitted values',
+plot( mod15$fitted.values, stud, pch = 16, xlab = 'Fitted values',
       ylab = 'Studentized Residuals', main = 'Studentized Residuals' )
-points( mod13$fitted.values[ watchout_ids_stud ], stud[ watchout_ids_stud ],
+points( mod15$fitted.values[ watchout_ids_stud ], stud[ watchout_ids_stud ],
         col = 'pink', pch = 16 )
-plot( mod13$fitted.values, lev, pch = 16, xlab = 'Fitted values',
+plot( mod15$fitted.values, lev, pch = 16, xlab = 'Fitted values',
       ylab = 'Leverages', main = 'Leverages' )
 
 points( mod13$fitted.values[ watchout_ids_lev ], lev[ watchout_ids_lev ],
@@ -294,11 +294,15 @@ points( mod13$fitted.values[ watchout_ids_lev ], lev[ watchout_ids_lev ],
 if(saveFig == TRUE){dev.off()}
 
 id_to_keep = !( 1:n %in% watchout_ids_Cdist )
-gl = lm( log(DIOX) ~ PLANT + TIME + LAB + NEFFEKT +  SO2 + HCL + H2O, dat[ id_to_keep, ] )
+
+gl = lm( log(DIOX) ~ PLANT + TIME + LAB + O2COR + NEFFEKT + SO2 + HCL, dat[ id_to_keep, ] )
 summary( gl )
 par(mfrow=c(2,2))
+logLik(gl)
 plot(gl)
-
+par(mfrow=c(2,2))
+plot(mod15)
+logLik(mod15)
 summary(mod15)
 
 #the improvement obtained not considering the points violating the CookDistance 
@@ -325,6 +329,7 @@ l<-function(sigma){
 
 nlminb(c(1,4), l, lower=c(0, 0), upper=c(Inf, Inf))
 logLik(mod15)
+anova(mo,mod15)
 
 
 # BONUS---> REDUCE MODEL WITH AIC/BIC
